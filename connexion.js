e9ba996db541425b49f0df88184e1b5e246136f0
Edouard Misset <edouardmisset@gmail.com>
2021-06-09 11:43:21 +0200

    created the getUsers and setRepo

---
+++ connexion.js
@@ -0,0 +1,33 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+// Function to return the users in the database
+async function getUsers() {
+  const users = await prisma.users.findMany();
+  console.log('new user: ', newUser, 'all users: ', users);
+  return users;
+}
+
+async function setRepo(repoList) {
+  const repos = await prisma.repositories.createMany(
+    {
+      data: repoList.map(repo => ({
+        url: repo.html_url,
+        user_id: repo.owner.id,
+      })),
+    },
+    { skipDuplicates: true }
+  );
+  return repos;
+}
+
+// main()
+//   .catch(e => {
+//     throw e;
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
+
+module.exports = { getUsers, setRepo };
