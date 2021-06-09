e02408b83a4cce5944b6bf6da7f009be511b4dd4
Edouard Misset <edouardmisset@gmail.com>
2021-06-09 10:16:29 +0200

    script to create the tables

--- reset-db.sql
+++ reset-db.sql
@@ -1,3 +1,6 @@
---@block Create a Table
+--@block Create a table users
 CREATE TABLE users (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, github_username VARCHAR(100) NOT NULL, repos_last_sync_date DATE NOT NULL);
+
+--@block Create a table repositories
+CREATE TABLE repositories (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, url VARCHAR(100) NOT NULL, user_id INT NOT NULL);
 
