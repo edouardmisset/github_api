20b65dcddf8dbadb97577bf5569ca9bedecbeb01
Edouard Misset <edouardmisset@gmail.com>
2021-06-09 11:19:57 +0200

    Added a sql command to create users
    Co-authored-by: DorianeGH <dorianeGH@users.noreply.github.com>

--- reset-db.sql
+++ reset-db.sql
@@ -4,3 +4,5 @@ CREATE TABLE users (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, github_username 
 --@block Create a table repositories
 CREATE TABLE repositories (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, url VARCHAR(100) NOT NULL, user_id INT NOT NULL);
 
+--@block Create a table repositories
+INSERT INTO users (github_username, repos_last_sync_date) VALUES ('torvalds', '2021-06-09' ), ('unclebob','2021-06-09');
